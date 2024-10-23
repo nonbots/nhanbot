8fef76c8836f95a592cda074402237453ee4b2e4
nonbots <nhantdbui@gmail.com>
2024-10-23 11:19:07 -0400

    websocket server listens to nhanify to queue song requests

---
+++ src/helper.js
@@ -0,0 +1,109 @@
+/**
+ * Is the URL input a valid url by hostname, pathname and is absolute.
+ * @params {String} url - The url input.
+ * @returns {boolean}
+ */export function isValidURL(URLInput) {
+  try {
+    const url = new URL(URLInput);
+    const protocol = url.protocol;
+    const hostname = url.hostname;
+    const pathname = url.pathname;
+    const videoId = url.searchParams.get("v");
+    if (protocol === "https:" || protocol === "http:") {
+      if (
+        (hostname === "www.youtube.com" && pathname === "/watch" && videoId) ||
+        (hostname === "youtu.be" && pathname) ||
+        (hostname === "m.youtube.com" && pathname === "/watch" && videoId)
+      )
+        return true;
+    }
+    return false;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+export async function getVidInfo(vidUrl, YT_API_KEY) {
+  const videoId = parseURL(vidUrl);
+  return await getVidInfoByVidId(videoId, YT_API_KEY);
+}
+
+async function getVidInfoByVidId(videoId, YT_API_KEY) {
+  const headers = { headers: { Accept: "application/json" } };
+  const url = new URL("https://www.googleapis.com/youtube/v3/videos");
+  url.search = new URLSearchParams({
+    key: YT_API_KEY,
+    id: videoId,
+    part: ["snippet", "contentDetails"],
+  }).toString();
+  const response = await fetch(url, headers);
+  const result = await response.json();
+  console.log("RESULTS", result);
+  console.log("ITEMS", result.items);
+  if (!result.items[0]) return null;
+  const duration = convertDuration(result.items[0].contentDetails.duration);
+  const durationSecs = convertToSec(duration);
+  const vidInfo = {
+    title: result.items[0].snippet.title,
+    videoId: result.items[0].id,
+    durationSecs,
+  };
+  return vidInfo;
+}
+function parseURL(URLInput) {
+  if (URLInput.includes("youtu.be")) {
+    let hostPath = URLInput.split("?")[0];
+    let path = hostPath.split("/");
+    return path[path.length - 1];
+  }
+  let queryStr = URLInput.split("?")[1];
+  let params = queryStr.split("&");
+  let videoIdParam = getVideoIdParams(params);
+  return videoIdParam.substring(2);
+}
+/**
+ * Gets the 'v' parameter
+ * @params {Array} - The collection of parameter strings.
+ * @returns {String} - A 'v' string parameter.
+ */
+function getVideoIdParams(params) {
+  for (let i = 0; i < params.length; i += 1) {
+    let param = params[i];
+    if (param.includes("v=")) return params[i];
+  }
+}
+export function durationSecsToHHMMSS(secs) {
+  const hrs = Math.floor(secs / 3600);
+  secs -= hrs * 3600;
+  const mins = Math.floor(secs / 60);
+  secs -= mins * 60;
+  const formatTime = `${hrs.toString().padStart(2, "0")}:${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
+  return formatTime;
+}
+function convertDuration(duration) {
+  const numStrs = ["00", "00", "00"];
+  const durationStr = duration.split("T")[1];
+  let numStr = "";
+
+  durationStr.split("").forEach((char) => {
+    if (!Number.isNaN(Number(char))) {
+      numStr += char;
+    } else {
+      if (char === "H" && numStr.length === 1) numStrs[0] = "0" + numStr;
+      if (char === "H" && numStr.length === 2) numStrs[0] = numStr;
+      if (char === "M" && numStr.length === 1) numStrs[1] = "0" + numStr;
+      if (char === "M" && numStr.length === 2) numStrs[1] = numStr;
+      if (char === "S" && numStr.length === 1) numStrs[2] = "0" + numStr;
+      if (char === "S" && numStr.length === 2) numStrs[2] = numStr;
+      numStr = "";
+    }
+  });
+  return numStrs.join(":");
+  //input "12:04:23"
+}
+
+function convertToSec(duration) {
+  //input "12:04:23"
+  const numArr = duration.split(":").map((numStr) => Number(numStr));
+  return (numArr[0] * 60 + numArr[1]) * 60 + numArr[2];
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import authInfo from "./auth.json" assert { type: 'json' }; // eslint-disable-line
+import {
+  durationSecsToHHMMSS,
+  isValidURL,
+  getVidInfo
+} from "./helper.js";
+
 import {writeFileSync } from 'node:fs';
 import websocket from "websocket";
-const { client: WebSocketClient } = websocket;
+import http from 'http';
+const { client: WebSocketClient, server: WebSocketServer } = websocket;
 import { CommandManager } from "./commandManager.js";
 import { createNewAuthToken, createFollowSubscription } from './accessToken.js';
 const commandManager = new CommandManager();
 import { isSentByStreamer } from "./permissions.js";
 
+const server = http.createServer((req, res) => {
+  res.writeHead(404);
+  res.end();
+});
+server.listen(8080, () => {
+  console.log("Websocket server is listening on ws://localhost:8080");
+});
+
 const ircClient = new WebSocketClient();
 const eventSubClient = new WebSocketClient();
+const nhanbotServer = new WebSocketServer({
+  httpServer: server,
+});
+
 const {
   TWITCH_TOKEN: password,
   REFRESH_TWITCH_TOKEN: refreshPassword,
@@ -19,12 +38,35 @@ const {
   NHANIFY_API_KEY,
   BOT_ID,
   BROADCASTER_ID,
+  YT_API_KEY,
 } = authInfo;
+
 const IRC_TOKEN = `oauth:${password}`
+const songQueue = [];
+const COOLDOWN_DURATION = 60 * 1000;
+let lastSongRequestTime = new Date();
 let IRC_connection;
 const moveMessage = "Get up and move, your body will thank you!";
 const defaultMoveInterval = 60000 * 60 * 1; // Set to 1 minute for testing.
 let moveInterval = defaultMoveInterval;
+
+nhanbotServer.on('request', (request) => {
+  const connection = request.accept(null, request.origin);
+  connection.on('message', (message) => {
+    if (message.type === 'utf8') {
+      const data = JSON.parse(message.utf8Data);
+      console.log({data});
+      if ((data.type === "playerStateEnded" || data.type === "playerStateStarted") && songQueue.length !== 0) {
+        console.log({songQueue});
+        const song = songQueue.shift();
+        connection.sendUTF(JSON.stringify(song));
+        IRC_connection.sendUTF(`PRIVMSG #${channel} : @${song.addedBy}, ${song.title} is now playing.`);
+      } else {
+        connection.sendUTF(JSON.stringify(null));
+      }
+    }
+  });
+});
 
 ircClient.on("connectFailed", function (error) {
   console.log("Connect Error: " + error.toString());
@@ -45,17 +87,17 @@ eventSubClient.on("connect", function (connection) {
                 console.log(`close description: ${connection.closeDescription}`);
                 let responseData = await createFollowSubscription(data.payload.session.id);
               if (responseData.message === 'Invalid OAuth token') {
-                  const data = await createNewAuthToken();
-                  const newToken = data.access_token;
-                  const newRefresh = data.refresh_token;
-                  authInfo.TWITCH_TOKEN = newToken;
-                  authInfo.REFRESH_TWITCH_TOKEN = newRefresh;
-                  writeFileSync("./src/auth.json", JSON.stringify(authInfo));
-                  //IRC_connection.close();
-                  //connection.close();
-                  //ircClient.connect("ws://irc-ws.chat.twitch.tv:80");
-                  //eventSubClient.connect("wss://eventsub.wss.twitch.tv/ws"); /// restart the program
-                }
+                const data = await createNewAuthToken();
+                const newToken = data.access_token;
+                const newRefresh = data.refresh_token;
+                authInfo.TWITCH_TOKEN = newToken;
+                authInfo.REFRESH_TWITCH_TOKEN = newRefresh;
+                writeFileSync("./src/auth.json", JSON.stringify(authInfo));
+                //IRC_connection.close();
+                //connection.close();
+                //ircClient.connect("ws://irc-ws.chat.twitch.tv:80");
+                //eventSubClient.connect("wss://eventsub.wss.twitch.tv/ws"); /// restart the program
+              }
             }else if (data.metadata.message_type === "session_reconnect") {
               oldConnection = connection 
               eventSubClient.connect(`${data.payload.session.reconnect_url}`);
@@ -76,7 +118,7 @@ ircClient.on("connect", function (connection) {
   connection.sendUTF(`NICK ${account}`);
   connection.sendUTF(`JOIN #${channel}`);
 
-  // to keep the connect by responsing back with a PONG
+ // to keep the connect by responsing back with a PONG
   connection.on("message", function(message) {
       if (message.type === 'utf8') {
         if (message.utf8Data.startsWith('PING :tmi.twitch.tv')) {
@@ -106,8 +148,15 @@ ircClient.on("connect", function (connection) {
     connection.sendUTF(`PRIVMSG ${message.command.channel} : ${result.playlists.length} public playlists`)
   })
   commandManager.addCommand("sr", async(message) => {
-    try {
+  //  try {
+      const addedBy = message.source.nick;
+      const timePassed = new Date() - lastSongRequestTime;
+      if (timePassed < COOLDOWN_DURATION) {
+        connection.sendUTF(`PRIVMSG ${message.command.channel} : @${addedBy}, ${Math.floor(COOLDOWN_DURATION / 1000) -  Math.floor(timePassed / 1000)} seconds more of cooldown.`);
+        return;
+      }
       const url = message.command.botCommandParams;
+     /*
       const playlistId = 6;
       const addedBy = message.source.nick;
       let payload = {
@@ -124,7 +173,26 @@ ircClient.on("connect", function (connection) {
           body: JSON.stringify(payload)
       });
     const result = await response.json();
-    switch(result.msg) {
+    */
+    if (!isValidURL(url)) {
+      connection.sendUTF(`PRIVMSG ${message.command.channel} : @${addedBy}, This url is invalid.`);
+      return;
+    }
+    const vidInfo = await getVidInfo(url, YT_API_KEY);
+    if (!vidInfo) {
+      connection.sendUTF(`PRIVMSG ${message.command.channel} : @${addedBy}, This video id is invalid.`);
+      return;
+    }
+    lastSongRequestTime = new Date();
+    songQueue.push({
+      title: vidInfo.title,
+      videoId: vidInfo.videoId,
+      duration: durationSecsToHHMMSS(vidInfo.durationSecs),
+      addedBy,
+    });
+    console.log({songQueue});
+    connection.sendUTF(`PRIVMSG ${message.command.channel} : @${addedBy}, ${vidInfo.title} was added to the queue.`);
+    /*switch(result.msg) {
       case 'success':
         connection.sendUTF(`PRIVMSG ${message.command.channel} : @${addedBy}, ${result.song.title} was added to "Twitch Stream" playlist.`);
         break;
@@ -137,12 +205,6 @@ ircClient.on("connect", function (connection) {
       case 'duplicate_video_id':
         connection.sendUTF(`PRIVMSG ${message.command.channel} : @${addedBy}, This song has already been added to the playlist.`);
         break;
-      case 'invalid_url':
-        connection.sendUTF(`PRIVMSG ${message.command.channel} : @${addedBy}, This url is invalid.`);
-        break;
-      case 'invalid_video_id':
-        connection.sendUTF(`PRIVMSG ${message.command.channel} : @${addedBy}, This video id is invalid.`);
-        break;
       default:
         connection.sendUTF(`PRIVMSG ${message.command.channel} : @${addedBy}, Oops! Something went wrong.`);
     }
@@ -150,6 +212,7 @@ ircClient.on("connect", function (connection) {
       console.error(error);
       connection.sendUTF(`PRIVMSG ${message.command.channel} : Oops! Nhanify is not available.`);
     }
+  */
   });
 
   commandManager.addCommand("ping", (message) => {
