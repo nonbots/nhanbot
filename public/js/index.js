6695561ba9aa1cbbf2b7df5d79476a0da56af3bc
nonbots <nhantdbui@gmail.com>
2024-10-28 01:04:24 -0400

    Feat: song request cooldown

--- public/js/index.js
+++ public/js/index.js
@@ -13,7 +13,6 @@ const curSongCard = document.getElementsByClassName('curSongCard')[0];
 const curSongImg = document.createElement('img');
 curSongImg.setAttribute("src", "../img/play.png");
 curSongImg.setAttribute("alt", "Playing");
-
 socket.onmessage = function (event) {
   try {
     const {songQueue, song, state} = JSON.parse(event.data);
@@ -22,8 +21,27 @@ socket.onmessage = function (event) {
       curSongCard.setAttribute("style", "padding:0rem");
     }
     if (state === "add_song") {
-      const cooldown = document.createElement('div');
-      cooldown.setAttribute("id", "cooldown");
+      console.log({state});
+      const cooldown = document.getElementsByClassName("cooldown")[0];
+      const text = document.getElementById("titleDisc");
+      let counter = 30;
+      const counterP = document.getElementById("counter");
+      console.log({counter});
+      const countInterval = setInterval(() => {
+        counter--;
+        counterP.textContent = counter;
+        if (counter <= 0) {
+          text.style.visibility = "visible";
+          counterP.style.visibility = "hidden";
+          clearInterval(countInterval);
+        }
+      }, 1000);
+      console.log({text});
+      text.style.visibility = 'hidden';
+      cooldown.style.animation = 'none';
+      cooldown.offsetWidth;
+      cooldown.style.backgroundColor = 'red';
+      cooldown.style.animation = 'roundtime calc(var(--duration) * 1s) linear forwards';
     }
     curSongCard.innerHTML = '';
     if (song) {
